6bff9f6bbdaddfa600767b8d0be7446e39f9a227
David Segovia <david.segovia@lts-beratung.de>
2024-09-23 18:39:26 +0200

    fix(ci): semantic release config branches

--- release.config.js
+++ release.config.js
@@ -2,7 +2,6 @@ const preset = "conventionalcommits";
 module.exports = {
   branches: [
     "master",
-    { name: "develop", prerelease: "beta" },
     { name: "release/*", prerelease: "rc", channel: "next" },
     { name: "hotfix/*", prerelease: "fix", channel: "hotfix" },
   ],
@@ -12,12 +11,12 @@ module.exports = {
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
     ["@semantic-release/github"],
     [
-      '@saithodev/semantic-release-backmerge',
+      "@saithodev/semantic-release-backmerge",
       {
         clearWorkspace: true,
-        backmergeStrategy: 'merge',
-        mergeMode: 'ours'
-      }
-    ]
+        backmergeStrategy: "merge",
+        mergeMode: "ours",
+      },
+    ],
   ],
 };
