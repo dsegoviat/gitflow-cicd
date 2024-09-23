4244b16e676e293f7df8befbf1360c051c5d78fc
David Segovia <david.segovia@lts-beratung.de>
2024-09-23 13:34:45 +0200

    fix: config pr

--- release.config.js
+++ release.config.js
@@ -4,12 +4,6 @@ module.exports = {
   plugins: [
     ["@semantic-release/commit-analyzer", { preset }],
     ["@semantic-release/release-notes-generator", { preset }],
-    [
-      "@semantic-release/exec",
-      {
-        publishCmd: "./release.sh ${nextRelease.version} ${branch.name}",
-      },
-    ],
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
     ["@semantic-release/github"]
   ],
