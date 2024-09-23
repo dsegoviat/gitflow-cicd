0001b94ee1367fd875a1a7e7a9e854e6e9568b3e
David Segovia <david.segovia@lts-beratung.de>
2024-09-23 12:49:29 +0200

    fix: semantic release config

--- release.config.js
+++ release.config.js
@@ -1,14 +1,14 @@
 const preset = "conventionalcommits";
-const branch = "develop";
 module.exports = {
-  branches: [branch],
+  branches: ["develop"],
   plugins: [
     ["@semantic-release/commit-analyzer", { preset }],
     ["@semantic-release/release-notes-generator", { preset }],
     [
       "semantic-release-github-pullrequest", {
-        "assets": ["CHANGELOG.md"],
-        "baseRef": branch
+        assets: ["CHANGELOG.md"],
+        baseRef: "main",
+        branch: "release/${nextRelease.version}"
       }
     ]
   ]
