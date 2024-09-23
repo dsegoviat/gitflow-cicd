3e4b39882900ae6312248391bf4fa3d7104bb8a7
David Segovia <david.segovia@lts-beratung.de>
2024-09-23 18:36:19 +0200

    feat(ci): add release process

--- release.config.js
+++ release.config.js
@@ -1,10 +1,23 @@
 const preset = "conventionalcommits";
 module.exports = {
-  branches: ["develop"],
+  branches: [
+    "master",
+    { name: "develop", prerelease: "beta" },
+    { name: "release/*", prerelease: "rc", channel: "next" },
+    { name: "hotfix/*", prerelease: "fix", channel: "hotfix" },
+  ],
   plugins: [
     ["@semantic-release/commit-analyzer", { preset }],
     ["@semantic-release/release-notes-generator", { preset }],
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
-    ["@semantic-release/github"]
+    ["@semantic-release/github"],
+    [
+      '@saithodev/semantic-release-backmerge',
+      {
+        clearWorkspace: true,
+        backmergeStrategy: 'merge',
+        mergeMode: 'ours'
+      }
+    ]
   ],
 };
